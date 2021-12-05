b965d2b67b7843b79e1e12bfd67feb2afb404d4e
zjm <zjm97@outlook.com>
2021-12-05 20:01:38 +0800

    收藏夹能够拖动

--- src/components/CollectionCharts.vue
+++ src/components/CollectionCharts.vue
@@ -1,14 +1,92 @@
 <template>
-    <n-space>
-        <n-card v-for="collection in collectionStore.collections">
-            <chart-raw :vegalite="collection"></chart-raw>
+    <grid-layout
+        :layout="SpecWithChart"
+        :col-num="12"
+        :row-height="150"
+        :is-draggable="true"
+        @layout-ready="onReady"
+        @update:layout="onLayoutChange"
+    >
+        <grid-item
+            v-for="(layout,idx) in SpecWithChart"
+            :key="layout.i"
+            :x="layout.x"
+            :y="layout.y"
+            :w="layout.w"
+            :h="layout.h"
+            :i="layout.i"
+            @resized="onResize(idx)"
+        >
+        <n-card style="height: 100%;width: 100%;">
+            <chart-raw
+                :vegalite="layout.spec"
+                :render-option="{
+                    width: 'container', height: 'container',
+                    autosize: {
+                        type: 'fit',
+                        contains: 'padding'
+                    },
+                    resize:true
+                    // contains:'padding',
+                    // config:{
+                    //     padding:20
+                    // }
+                }"
+                :ref="el => { if (el) charts[idx] = el }"
+                style="width:100%;height:100%"
+            ></chart-raw>
         </n-card>
-    </n-space>
+        </grid-item>
+    </grid-layout>
 </template>
 
 <script setup>
-import { NSpace,NCard } from 'naive-ui';
+import { NSpace, NCard,NScrollbar } from 'naive-ui';
 import ChartRaw from './ChartRaw.vue';
 import { CollectionStore } from '../store/CollectionStore';
+
+import { computed, toRaw, ref } from "vue-demi";
+import { onBeforeUpdate } from "vue";
+import _ from "lodash";
+
 const collectionStore = CollectionStore();
+
+const SpecWithChart = computed(() => {
+    return collectionStore.collections.map(collection => {
+        const id = collectionStore.specIds[JSON.stringify(collection)];
+        return {
+            spec: collection,
+            ...collectionStore.layouts.find(i => i.i === id),
+        }
+    })
+})
+
+const charts = ref([]);
+
+onBeforeUpdate(function () {
+    charts.value = [];
+})
+
+function onResize(idx) {
+
+    charts.value[idx].resize();
+}
+
+function onReady(){
+    for(let chart of charts.value){
+        chart.refreshChart();
+    }
+}
+
+function updateLayout(value){
+    collectionStore.layouts = value.map(i => ({
+        i: i.i,
+        x: i.x,
+        y: i.y,
+        w: i.w,
+        h: i.h
+    }));
+}
+
+const onLayoutChange=_.debounce(updateLayout,500);
 </script>
--- src/store/CollectionStore.js
+++ src/store/CollectionStore.js
@@ -2,10 +2,22 @@ import { defineStore } from "pinia";
 
 import _ from "lodash";
 
+let idx=0;
+
+function findUseableLayout(layouts,size={w:2,h:2}){
+    return {
+        x:0,
+        y:_(layouts).map(l=>l.y).max()||0,
+        ...size
+    }
+}
+
 export const CollectionStore = defineStore({
     id: "CollectionStore",
     state: () => ({
         collections: [],
+        layouts: [],
+        specIds:{}
     }),
     getters: {
         inCollection: state => {
@@ -20,13 +32,25 @@ export const CollectionStore = defineStore({
                 return;
             }
             else{
+                const strSpec = JSON.stringify(spec);
+                if(!_.has(this.specIds,strSpec)){
+                    this.specIds[strSpec] = idx++;
+                }
                 this.collections.push(_.cloneDeep(spec));
+                this.layouts.push({
+                    ...findUseableLayout(this.layouts),
+                    i:this.specIds[strSpec]
+                });
             }
         },
         remove(spec){
             if(this.inCollection(spec)){
                 this.collections = _.filter(this.collections, (c)=>{
                     return !_.isEqual(c, spec);
+                });
+                const strSpec = JSON.stringify(spec);
+                this.layouts = _.filter(this.layouts, (l)=>{
+                    return l.i!==this.specIds[strSpec];
                 });
             }
         }
