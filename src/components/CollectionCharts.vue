c7a519f24f52cf13d7784b6b844f82831ad27742
zjm <zjm97@outlook.com>
2022-03-23 18:15:30 +0800

    Update CollectionCharts.vue

--- src/components/CollectionCharts.vue
+++ src/components/CollectionCharts.vue
@@ -1,37 +1,37 @@
 <template>
-    <grid-layout
-        :layout="SpecWithChart"
-        :col-num="12"
-        :row-height="150"
-        :is-draggable="true"
-        @layout-ready="onReady"
-        @update:layout="onLayoutChange"
-        :verticalCompact="false" 
-    >
-        <grid-item
-            v-for="(layout, idx) in SpecWithChart"
-            :key="layout.i"
-            :x="layout.x"
-            :y="layout.y"
-            :w="layout.w"
-            :h="layout.h"
-            :i="layout.i"
-            drag-ignore-from=".dont_move"
-            @resized="resizedEvent"
-            @click.stop="onClickItem(layout)"
-
+    <div>
+        <grid-layout
+            :layout="SpecWithChart"
+            :col-num="12"
+            :row-height="150"
+            :is-draggable="true"
+            @layout-ready="onReady"
+            @update:layout="onLayoutChange"
+            :verticalCompact="false"
         >
-            <!-- <n-card 
+            <grid-item
+                v-for="(layout, idx) in SpecWithChart"
+                :key="layout.i"
+                :x="layout.x"
+                :y="layout.y"
+                :w="layout.w"
+                :h="layout.h"
+                :i="layout.i"
+                drag-ignore-from=".dont_move"
+                @resized="resizedEvent"
+                @click.stop="onClickItem(layout)"
+            >
+                <!-- <n-card 
                 style="height: 100%;width: 100%;" 
                 :class="{'shadow-lg':controlStore.currentViewId==layout.i, 'shadow-blue-500':true}"
-            > -->
-            <n-card 
-                style="height: 100%;width: 100%;" 
-                :class="{'!outline-$primary-color !outline-3':controlStore.currentViewId==layout.i}"
-                class="outline-none transition-all duration-300 border-3"
-            >
-                <template #header>Chart#{{ layout.i }}</template>
-                <!-- <template #header-extra>
+                >-->
+                <n-card
+                    style="height: 100%;width: 100%;"
+                    :class="{ '!outline-$primary-color !outline-3': controlStore.currentViewId == layout.i }"
+                    class="outline-none transition-all duration-300 border-3"
+                >
+                    <template #header>Chart#{{ layout.i }}</template>
+                    <!-- <template #header-extra>
                     <n-space>
                         <n-popover trigger="click" @update:show="onToggle(layout.spec)">
                             <template #trigger>
@@ -57,28 +57,29 @@
                             </n-icon>
                         </n-button>
                     </n-space>
-                </template>-->
+                    </template>-->
 
-                <chart-raw
-                    class="dont_move"
-                    @addview="addviews($event,layout.i)"
-                    :vegalite="layout.spec.spec"
-                    :render-option="{
-                        width: 'container', height: 'container',
-                        autosize: {
-                            type: 'fit',
-                            contains: 'padding'
-                        },
-                        resize: true
-                    }"
-                    v-if="chart_enabled"
-                    :ref="el => { if (el) charts[idx] = el }"
-                    style="width:100%;height:100%"
-                    :replaceColor="true"
-                ></chart-raw>
-            </n-card>
-        </grid-item>
-    </grid-layout>
+                    <chart-raw
+                        class="dont_move"
+                        @addview="addviews($event, layout.i)"
+                        :vegalite="layout.spec.spec"
+                        :render-option="{
+                            width: 'container', height: 'container',
+                            autosize: {
+                                type: 'fit',
+                                contains: 'padding'
+                            },
+                            resize: true
+                        }"
+                        v-if="chart_enabled"
+                        :ref="el => { if (el) charts[idx] = el }"
+                        style="width:100%;height:100%"
+                        :replaceColor="true"
+                    ></chart-raw>
+                </n-card>
+            </grid-item>
+        </grid-layout>
+    </div>
 </template>
 
 <script setup>
@@ -117,16 +118,16 @@ const SpecWithChart = computed(() => {
         // const strSpec = JSON.stringify(collection)
         // const id = collectionStore.specIds[strSpec];
         const id = collection.id;
-        if(collectionStore.exists[id]==1)
+        if (collectionStore.exists[id] == 1)
             return {
                 spec: collection,
                 // note: collectionStore.notes[strSpec],
                 ...collectionStore.layouts.find(i => i.i === id),
             }
     })
-    let res2=[];
-    for(let i=0;i<res.length;i++){
-        if(res[i])
+    let res2 = [];
+    for (let i = 0; i < res.length; i++) {
+        if (res[i])
             res2.push(res[i])
     }
     // console.log(res2)
@@ -136,54 +137,54 @@ const SpecWithChart = computed(() => {
 const charts = ref([]);
 
 // const views = ref([]);
-const views=ref({});
+const views = ref({});
 
 function onBrush(nowi) {
     console.log(111)
-  let data = views.value[nowi].getState().data;
-  for (let key in views.value) {
-// for(let key=0;key<views.value.length;key++){
-    if(key!=nowi){
-      views.value[key].setState({
-        data: data,
-        signals: views.value[key].getState().signals
-      })
+    let data = views.value[nowi].getState().data;
+    for (let key in views.value) {
+        // for(let key=0;key<views.value.length;key++){
+        if (key != nowi) {
+            views.value[key].setState({
+                data: data,
+                signals: views.value[key].getState().signals
+            })
+        }
     }
-  }
-  console.log(views.value)
+    console.log(views.value)
 }
 function clear(now) {
 
-  for (let i = 0; i < viewscnt.value; i++) {
-    if (i != now) {
-      try {
-        console.log(views.value[0]);
-        let d = views.value[i].getState().data;
-        let s = views.value[i].getState().signals;
-        // s.brush={};
-        // s.brush_tuple=null;
-        s.brush_x = [];
-        s.brush_y = [];
-        views.value[i].setState({
-          data: d,
-          signals: s
-        })
-      }
-      catch (err) {
-         console.error('request error', err);
-      }
+    for (let i = 0; i < viewscnt.value; i++) {
+        if (i != now) {
+            try {
+                console.log(views.value[0]);
+                let d = views.value[i].getState().data;
+                let s = views.value[i].getState().signals;
+                // s.brush={};
+                // s.brush_tuple=null;
+                s.brush_x = [];
+                s.brush_y = [];
+                views.value[i].setState({
+                    data: d,
+                    signals: s
+                })
+            }
+            catch (err) {
+                console.error('request error', err);
+            }
+        }
     }
-  }
 }
-function addviews(v,i){
+function addviews(v, i) {
 
-    views.value[i]=v;
+    views.value[i] = v;
     // views.value.push(v)
     console.log(views.value);
     v.addSignalListener("brush",
-      _.debounce(() => {
-        onBrush(i);
-      }, 500)
+        _.debounce(() => {
+            onBrush(i);
+        }, 500)
     )
     // v.addEventListener("mouseup", () => {
     //   // if(checkfirst()==0)
@@ -301,5 +302,4 @@ function specify(spec) {
 .vue-grid-item.vue-grid-placeholder {
     background: var(--light-primary-color) !important;
 }
-
 </style>
